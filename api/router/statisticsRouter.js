f5b76af7847e8629dcad881eb7c8684e570131bf
piapip <tho.ph153619@sis.hust.edu.vn>
2019-11-26 22:48:51 +0700

    Sua ham statistic, them 1 chuc nang

--- api/router/statisticsRouter.js
+++ api/router/statisticsRouter.js
@@ -17,4 +17,21 @@ statisticsRouter.get("/get-latest", (req, res) => {
     .catch(error => res.status(400).err(error))
 })
 
+statisticsRouter.get("/get-latest/:productName", (req, res) => {
+  axios
+    .get("https://nguyenvd27-ltct-demo.herokuapp.com/api/products/")
+    .then(data => {
+
+      let filteredData = data.data.data.filter(element => element.name.toLowerCase().includes(req.params.productName.toLowerCase()))
+      let sortedData = Object.keys(filteredData).sort((a,b) => {
+        return filteredData[a].created_at - filteredData[b].created_at
+      })
+      let result = []
+      for (key in sortedData)
+        result.push(filteredData[sortedData[key]])
+      res.status(500).send(result)
+    })
+    .catch(error => res.status(400).err(error))
+})
+
 module.exports = statisticsRouter;
