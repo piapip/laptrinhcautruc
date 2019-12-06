60957df124a3cffce60186f5ab720ca27c31c0bf
baohoangbk235 <baohoangbk235@gmail.com>
2019-12-06 16:22:29 +0700

    fix url

--- testing/src/config.js
+++ testing/src/config.js
@@ -3,6 +3,6 @@ export default {
   NHOM2: 'http://secure-mountain-93147.herokuapp.com',
   NHOM3: 'https://nguyenvd27-ltct-demo.herokuapp.com',
   NHOM7: '',
-  BACKEND_NHOM9: 'http://localhost:8080',
-  FRONTEND_NHOM9: 'http://localhost:3000'
+  BACKEND_NHOM9: 'https://laptrinhcautruc.herokuapp.com',
+  FRONTEND_NHOM9: 'http://frontendltct.herokuapp.com'
 };
