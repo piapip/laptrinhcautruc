901ca88e339e09f5ad20c40e84931d7784a1e51c
piapip <tho.ph153619@sis.hust.edu.vn>
2019-12-06 18:56:37 +0700

    Ket noi voi nhom 7.

--- testing/src/config.js
+++ testing/src/config.js
@@ -2,7 +2,7 @@ export default {
   NHOM1: 'https://shopsp01.herokuapp.com',
   NHOM2: 'http://secure-mountain-93147.herokuapp.com',
   NHOM3: 'https://nguyenvd27-ltct-demo.herokuapp.com',
-  NHOM7: '',
+  NHOM7: 'https://nhom6qtch.herokuapp.com/api/search/1',
   BACKEND_NHOM9: 'http://localhost:8080',
   FRONTEND_NHOM9: 'http://localhost:3000'
 };
